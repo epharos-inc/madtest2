28995b801a08e7c96b5bc03997ca759285d68e44
epharos-inc <54557003+epharos-inc@users.noreply.github.com>
2022-03-13 09:42:46 -0500

    Create Test1.java

---
+++ Test1.java
@@ -0,0 +1,12 @@
+
+// First commit NR
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
